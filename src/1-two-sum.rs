26e89bff470a482e8c1300abdacb6aefcaf2f4f5
MizukiCry <lanrtabe@qq.com>
2022-08-21 23:57:01 +0800

    finish leetcode-1

---
+++ src/1-two-sum.rs
@@ -0,0 +1,15 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = HashMap::new();
+
+        for (index, value) in nums.iter().enumerate() {
+            if let Some(&p) = map.get(&(target - value)) {
+                return vec![p, index as i32];
+            }
+            map.insert(value, index as i32);
+        }
+        return vec![];
+    }
+}
