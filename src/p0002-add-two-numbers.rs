61fe3c5e4935b02949a64bb1c8b0b749f5e50238
MizukiCry <lanrtabe@qq.com>
2022-08-22 23:18:06 +0800

    finish leetcode-2

---
+++ src/p0002-add-two-numbers.rs
@@ -0,0 +1,37 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+//
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+
+fn add(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>, mut v: i32) -> Option<Box<ListNode>> {
+    if l1.is_none() && l2.is_none() && v == 0 {
+        None
+    } else {
+        Some(Box::new(ListNode {
+            next: add(
+                l1.and_then(|x| {v += x.val; x.next}),
+                l2.and_then(|x| {v += x.val; x.next}),
+                v / 10
+                ),
+            val: v % 10
+        }))
+    }
+}
+
+impl Solution {
+    pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        add(l1, l2, 0)
+    }
+}
