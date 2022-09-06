d544a9e1dc251aaac15c949757fa36b750d7f8e5
MizukiCry <lanrtabe@qq.com>
2022-09-07 00:07:41 +0800

    1

---
+++ src/bin/p0030-substring-with-concatenation-of-all-words.rs
@@ -0,0 +1,64 @@
+use std::{collections::HashMap, cell::Cell};
+
+impl Solution {
+    pub fn find_substring(s: String, words: Vec<String>) -> Vec<i32> {
+        const HASH_BASE: i64 = 131;
+        const HASH_MOD: i64 = 998244353;
+
+        let s = s.as_bytes();
+        let mut s_hash: Vec<i64> = Vec::with_capacity(s.len());
+        for i in 0..s.len() {
+            s_hash[i] = (s[i] as i64 * HASH_BASE + if i == 0 { 0 } else { s[i - 1] as i64 }) % HASH_MOD;
+        }
+
+        let word_len = words.first().unwrap().len();
+        let total_len = word_len * words.len();
+        let mut words_hash: HashMap<i64, i32> = HashMap::with_capacity(words.len());
+        for w in words {
+            let mut hash_val = 0;
+            for c in w.bytes() {
+                hash_val = (hash_val * HASH_BASE + c as i64) % HASH_MOD;
+            }
+            words_hash.entry(hash_val).and_modify(|x| *x += 1).or_insert(1);
+        }
+
+        let mut hash_pow: i64 = 1;
+        for _ in 0..word_len {
+            hash_pow = hash_pow * HASH_BASE % HASH_MOD;
+        }
+
+        let mut unmatch_num = Cell::new(words_hash.len());
+        let mut modify = |hash_val: i64, num: i32| {
+            if let Some(i) = words_hash.get_mut(&hash_val) {
+                if *i == 0 {
+                    *unmatch_num.get_mut() += 1;
+                }
+                *i += num;
+                if *i == 0 {
+                    *unmatch_num.get_mut() -= 1;
+                }
+            }
+        };
+
+        let calc_hash = |pos: usize| -> i64 {
+            (s_hash[pos + word_len - 1] - (if pos == 0 { 0 } else { s_hash[pos - 1] }) * hash_pow % HASH_MOD + HASH_MOD) % HASH_MOD
+        };
+
+        let mut ans: Vec<i32> = vec![];
+
+        for i in 0..word_len {
+            let mut j = i;
+            while j + word_len <= s.len() {
+                modify(calc_hash(j), -1);
+                if j >= total_len {
+                    modify(calc_hash(j - total_len), 1);
+                }
+                if unmatch_num.get() == 0 {
+                    ans.push((j - total_len + word_len) as i32);
+                }
+            }
+        }
+
+        ans
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
+use std::{cell::{Cell, RefCell}, rc::Rc, borrow::BorrowMut};
+
 fn main() {
-    for i in (1..5).rev() {
-        println!("{}", i);
+
+    {
+        let s = Rc::new(RefCell::new("我很善变，还拥有多个主人".to_string()));
+
+        let s1 = s.clone();
+        let s2 = s.clone();
+        // let mut s2 = s.borrow_mut();
+        s2.borrow_mut().push_str(", on yeah!");
+
+        println!("{:?}\n{:?}\n{:?}", s, s1, s2);
     }
-    println!("Hello, world!");
+
+    /*let mut x = Rc::new(Cell::new(1));
+    let t = x.clone();
+    t.borrow_mut() += 1;
+
+    let mut func = || {
+        
+        *t.borrow_mut().get_mut() += 1;
+    };
+
+    println!("{}", x.get());
+    func();*/
+
 }
